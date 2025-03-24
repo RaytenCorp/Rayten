8209508dc604b9d33787d0ec82508eba2063c08b
Bamgeglo1485 <akkauntmin@gmail.com>
2025-03-24 19:52:07 +0900

    sdsfsadfsdf

--- Content.Server/Roles/R1RoleComponent.cs
+++ Content.Server/Roles/R1RoleComponent.cs
@@ -1,4 +1,3 @@
-using Content.Server.Dragon;
 using Content.Shared.Roles;
 
 namespace Content.Server.Roles;
@@ -6,7 +5,7 @@ namespace Content.Server.Roles;
 /// <summary>
 ///     Added to mind role entities to tag that they are a R1.
 /// </summary>
-[RegisterComponent, Access(typeof(DragonSystem))]
+[RegisterComponent]
 public sealed partial class R1RoleComponent : BaseMindRoleComponent
 {
 }
