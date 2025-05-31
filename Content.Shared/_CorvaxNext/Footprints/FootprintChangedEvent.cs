4469449f4a64b5e2f0b51dab8ec882e66494bfdd
TFMB <mail@vanilla-station.ru>
2025-05-31 11:24:41 +0300

    Revert "FootprintsRework (#132)"
    This reverts commit 65ca0cd2ef477254c356c468229ad429a7a887b4.

--- Content.Shared/_CorvaxNext/Footprints/FootprintChangedEvent.cs
+++
@@ -1,9 +0,0 @@
-using Robust.Shared.Serialization;
-
-namespace Content.Shared._CorvaxNext.Footprints;
-
-[Serializable, NetSerializable]
-public sealed class FootprintChangedEvent(NetEntity entity) : EntityEventArgs
-{
-    public NetEntity Entity = entity;
-}
