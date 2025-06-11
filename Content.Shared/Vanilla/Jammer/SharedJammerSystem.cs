b40f7e50369d3b1e20f70c6f43c874a1d4e10ca2
TFMB <mail@vanilla-station.ru>
2025-06-11 22:21:51 +0300

    энергощиты по швам затрещали

---
+++ Content.Shared/Vanilla/Jammer/SharedJammerSystem.cs
@@ -0,0 +1,19 @@
+using Robust.Shared.Network;
+using Robust.Shared.Serialization;
+using Robust.Shared.Timing;
+
+namespace Content.Shared.Vanilla.Jammer;
+
+[DataDefinition]
+public sealed partial class OverrideJammerTimeEvent : BackgroundEvent
+{
+    [DataField("minutes")]
+    public float Minutes = 5;
+
+    public OverrideJammerTimeEvent() { }
+
+    public OverrideJammerTimeEvent(float minutes)
+    {
+        Minutes = minutes;
+    }
+}
