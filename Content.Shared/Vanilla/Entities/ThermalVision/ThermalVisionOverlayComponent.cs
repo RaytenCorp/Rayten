9b23ec7aac0a88d0767c413cfea4e3c65a9b96ef
TFMB <mail@vanilla-station.ru>
2025-08-05 23:30:41 +0300

    прототипы готовы

--- Content.Shared/Vanilla/Entities/ThermalVision/ThermalVisionOverlayComponent.cs
+++
@@ -1,8 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared.Vanilla.Entities.ThermalVision;
-
-[RegisterComponent, NetworkedComponent]
-public sealed partial class ThermalVisionOverlayComponent : Component
-{
-}
