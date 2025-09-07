d5a3bc8c4c54831b7e855121ba9f528c9f5f273d
TFMB <mail@vanilla-station.ru>
2025-09-07 16:11:27 +0300

    piu piu piu

--- Content.Server/Vanilla/EventTeam/StationERTComponent.cs
+++ Content.Server/Vanilla/EventTeam/StationERTComponent.cs
@@ -11,7 +11,7 @@ public sealed partial class StationERTComponent : Component
 {
 
     [DataField]
-    public ResPath Map = new("Maps/Vanilla/Maps/VerfDSO.yml");
+    public ResPath Map = new("/Maps/Vanilla/Maps/VerfDSO.yml");
 
     [DataField]
     public EntityUid? Entity;
