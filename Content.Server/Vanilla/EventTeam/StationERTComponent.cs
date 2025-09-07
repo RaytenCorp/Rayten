6f6456e41ce53eaf59d8f9fef8665c3b1555fe11
TFMB <mail@vanilla-station.ru>
2025-09-07 16:04:32 +0300

    verfdso

---
+++ Content.Server/Vanilla/EventTeam/StationERTComponent.cs
@@ -0,0 +1,24 @@
+using Robust.Shared.Map;
+using Robust.Shared.Utility;
+
+namespace Content.Server.Vanilla.EventTeam;
+
+/// <summary>
+/// Спавнит верфь ДСО
+/// </summary>
+[RegisterComponent]
+public sealed partial class StationERTComponent : Component
+{
+
+    [DataField]
+    public ResPath Map = new("Maps/Vanilla/Maps/VerfDSO.yml");
+
+    [DataField]
+    public EntityUid? Entity;
+
+    [DataField]
+    public EntityUid? MapEntity;
+
+    [DataField]
+    public bool ERTCalled = false;
+}
