8326d1de9328a3711b9d7ae3ff5b85e2c82eae01
Lemniscate <mail@rayten.com>
2025-11-14 02:55:04 +0300

    перчики и нетсинкинг

--- Content.Shared/Vanilla/Entities/Clothing/BlueSpaceSync/BlueSpaceSyncComponent.cs
+++ Content.Shared/Vanilla/Entities/Clothing/BlueSpaceSync/BlueSpaceSyncComponent.cs
@@ -1,9 +1,11 @@
+using Robust.Shared.GameStates;
+
 namespace Content.Shared.Vanilla.Entities.BlueSpaceSync;
 ///маркер для того чтобы десинхронизировать челика обратно
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class BlueSpaceSyncComponent : Component
 {
-    [DataField]
+    [DataField, AutoNetworkedField]
     public TimeSpan EscapeTime = TimeSpan.Zero;
 
     [DataField]
