175686c22725ef10212405aaf7e224d63704d765
Lemniscate <mail@rayten.com>
2026-01-04 00:45:10 +0300

    музыка для всех!

--- Content.Shared/Vanilla/RoleSkills/RoleSkillsPrototype.cs
+++ Content.Shared/Vanilla/RoleSkills/RoleSkillsPrototype.cs
@@ -16,4 +16,7 @@ public sealed partial class RoleSkillsPrototype : IPrototype
 
     [DataField]
     public HashSet<SkillType> EasySkills = [];
+
+    [DataField]
+    public int SkillPoints = 8;
 }
